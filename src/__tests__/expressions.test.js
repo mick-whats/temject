438334eead983ebcce19940e2ff4160e0ac74580
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-14 19:56:31 +0900

    add args "opts"

--- src/__tests__/expressions.test.js
+++ src/__tests__/expressions.test.js
@@ -5,6 +5,21 @@ test('expressions test', () => {
   const res = expressions(str)
   expect(res).toEqual(['name', 'age'])
 })
+test('expressions ignore string', () => {
+  const str = '{{name}}{{name:pascal}}{{age}}{{foo}}{{bar}}'
+  const res = expressions(str, { ignore: 'name' })
+  expect(res).toEqual(['age', 'foo', 'bar'])
+})
+test('expressions ignore arr', () => {
+  const str = '{{name}}{{name:pascal}}{{age}}{{foo}}{{bar}}'
+  const res = expressions(str, { ignore: ['name', 'age'] })
+  expect(res).toEqual(['foo', 'bar'])
+})
+test('expressions ignore unknown type', () => {
+  const str = '{{name}}{{name:pascal}}{{age}}{{foo}}{{bar}}'
+  // const res = expressions(str, { ignore: {} })
+  expect(() => { expressions(str, { ignore: {} }) }).toThrow()
+})
 test('not expressions', () => {
   const str = 'test string'
   const res = expressions(str)
--- src/expressions.js
+++ src/expressions.js
@@ -3,16 +3,36 @@
  * Returns a list of template expressions
  *
  * @param {string} str - basic template strings
+ * @param {Object} opts - options
  * @return {Array}
  * @example
  * const str = '{{name}}{{name:pascal}}{{age}}'
  * const res = expressions(str)
  * // -> [ 'name', 'age' ]
  */
-module.exports = function expressions (str) {
+module.exports = function expressions (str, opts = {}) {
   const RGX = /{{(.*?)}}/g
   const m = str.match(RGX)
-  return m
+  const ignore = (() => {
+    if (opts.ignore && typeof opts.ignore === 'string') {
+      return [opts.ignore]
+    } else if (Array.isArray(opts.ignore)) {
+      return opts.ignore
+    } else if (opts.ignore) {
+      throw new Error('options.ignore is unmatch type')
+    } else {
+      return []
+    }
+  })()
+  // const ignore = opts.ignore
+  //   ? typeof opts.ignore === 'string'
+  //     ? [opts.ignore]
+  //     : Array.isArray(opts.ignore)
+  //     ? opts.ignore
+  //     : throw new Error('options.ignore is unmatch type')
+  //   : null
+
+  const arr = m
     ? str
       .match(RGX)
       .map(item => {
@@ -23,4 +43,9 @@ module.exports = function expressions (str) {
       })
       .filter((x, i, me) => me.indexOf(x) === i)
     : []
+  return ignore.length
+    ? arr.filter(item => {
+      return !ignore.includes(item)
+    })
+    : arr
 }
